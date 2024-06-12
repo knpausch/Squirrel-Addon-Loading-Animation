404db67a7a2d8eaff0adb47d58eb04f024d7298d
Keyaanna (Kiko) Pausch <knpausch@gmail.com>
2024-06-12 16:37:19 -0700

    improve styling of animations, added dice animation, added js logic to update animation type, color, and opacity

--- scripts/app.js
+++ scripts/app.js
@@ -1,31 +1,31 @@
 (() => {
     "use strict";
 
-    // listen to Squirrel lifecycle events
     Squirrel.addEventListener('eventDispatch', (e) => eval(`${e.detail.name}(e)`));
 
-    // initialise 
     Squirrel.initWithSquirrel();
 
-    /**
-   * Take a string, to be shown in the HTML display, capitalise 
-   * the letters and then send back to Squirrel
-   * @param value The string to display and return
-   */
-    function processData(value) {
-        document.getElementById('helloWorldText').textContent = value;
-        Squirrel.sendToSquirrel('helloWorldResponse', value.toUpperCase());
-
-    }
-
-    // handles a property change sent from Squirrel. 
     function onPropertyChange(e) {
         const propertyName = e.detail.property
         const propertyValue = e.detail.value
 
         switch (Squirrel.getGenericProperty(propertyName)) {
-            case 'helloWorldData':
-                processData(propertyValue)
+            case 'animationType':
+                if(propertyValue == "spinner"){
+                    document.getElementById('loader').className = "spinner"
+                }
+                if(propertyValue == "dots"){
+                    document.getElementById('loader').className = "dots --3"
+                }
+                if(propertyValue == "dice"){
+                    document.getElementById('loader').className = "dice --3"
+                }
+                break;
+            case 'animationColor.color.*.color':
+                document.getElementById('loader').style.setProperty('--color', propertyValue)
+                break;
+            case 'animationColor.color.*.alpha':
+                document.getElementById('loader').style.setProperty('opacity', propertyValue)
                 break;
             default:
                 console.log("Unknown message type: " + propertyName);
@@ -33,43 +33,39 @@
         }
     }
 
-    // handles setting the initial setate sent from Squirrel.
     function onInitState(e) {
-        //set the state from the message
         const state = e.detail.state
 
-        if (state != null) {
-            processData(state.helloWorldData);
-        }
+        document.getElementById('loader').style.width = window.innerWidth + 'px';
+        document.getElementById('loader').style.height = window.innerHeight + 'px';
     }
 
-    /**
-     * Called at the end of a series of property value changes.  This can be called
-     * either when a single or multiple values change at once.  This is the flag to say
-     * There are no more incoming value changs to process at this time.
-     */
-    function onPropertyChangesComplete() { }
+    function onPropertyChangesComplete() {}
 
-    // Called when a setCanvas event is received from Squirrel
     function onSetCanvas(e) {
         const canvas = e.detail.canvas;
     }
 
-    // Called when a setRuntimeMode event is received from Squirrel
     function onSetRuntimeMode(e) {
         const mode = e.detail.mode;
     }
 
-    // Called when a setSize event is received from Squirrel
     function onSetSize(e) {
         const size = e.detail.size;
+        document.getElementById('loader').style.width = window.innerWidth + 'px';
+        document.getElementById('loader').style.height = window.innerHeight + 'px';
+
+        // if(size.width != document.getElementById('loader').offsetWidth || size.height != document.getElementById('loader').offsetHeight) {
+        //     processSize()
+        // }
     }
 
-    // Called when a setPosition event is received from Squirrel
     function onSetPosition(e) {
         const position = e.detail.position;
     }
 
-
-
+    // function processSize() {
+    //     Squirrel.setSize(new SquirrelSize(document.getElementById('loader').style.offsetWidth));
+    //     Squirrel.setSize(new SquirrelSize(document.getElementById('loader').style.offsetHeight));
+    // }
 })();
