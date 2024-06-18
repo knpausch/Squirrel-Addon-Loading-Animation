7b6aea6ecd1f604578b73afd831855551f470f89
Keyaanna (Kiko) Pausch <knpausch@gmail.com>
2024-06-18 16:05:21 -0700

    Fixed stateless issue, now app remembers animation selected in any mode, removed selection gif for now, refactored code, created render function

--- scripts/app.js
+++ scripts/app.js
@@ -5,61 +5,31 @@
 
     Squirrel.initWithSquirrel();
 
+    var animationSelected;
+
+    function renderAnimation() {
+        document.getElementById('loader').className = animationSelected
+    }
+
+    function onInitState(e) {
+        const state = e.detail.state
+        Squirrel.setSize(200, 200)
+        
+        if(state != null) {
+            animationSelected = state.animationType
+            renderAnimation()
+        }
+    }
+
     function onPropertyChange(e) {
         const propertyName = e.detail.property
         const propertyValue = e.detail.value
 
         switch (Squirrel.getGenericProperty(propertyName)) {
             case 'animationType':
-                if(propertyValue == "please-select"){
-                    document.getElementById('select-img').style.display = "block"
-                    document.getElementById('loader').className = ""
-                    break;
-                }
-                if(propertyValue == "spinner"){
-                    document.getElementById('loader').className = "spinner"
-                }
-                if(propertyValue == "dribble"){
-                    document.getElementById('loader').className = "dribble"
-                }
-                if(propertyValue == "side-step"){
-                    document.getElementById('loader').className = "side-step"
-                }
-                if(propertyValue == "roadway"){
-                    document.getElementById('loader').className = "roadway"
-                }
-                if(propertyValue == "inchworm"){
-                    document.getElementById('loader').className = "inchworm"
-                }
-                if(propertyValue == "rain"){
-                    document.getElementById('loader').className = "rain"
-                }
-                if(propertyValue == "square-dance"){
-                    document.getElementById('loader').className = "square-dance"
-                }
-                if(propertyValue == "shuffle"){
-                    document.getElementById('loader').className = "shuffle"
-                }
-                if(propertyValue == "firework"){
-                    document.getElementById('loader').className = "firework"
-                }
-                document.getElementById('select-img').style.display = "none"
-                break;
-            case 'animationColor.color.*.color':
-                document.getElementById('loader').style.setProperty('--color', propertyValue)
-                break;
-            case 'animationColor.color.*.alpha':
-                document.getElementById('loader').style.setProperty('opacity', propertyValue)
-                break;
-            default:
-                console.log("Unknown message type: " + propertyName);
-                break;
+                animationSelected = propertyValue
         }
-    }
-
-    function onInitState(e) {
-        const state = e.detail.state
-        Squirrel.setSize(200, 200)
+        renderAnimation()
     }
 
     function onPropertyChangesComplete() {}
